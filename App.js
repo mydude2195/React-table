2ff286e516f9c954baee0b04f89f431771673131
mydude2195 <59919472+mydude2195@users.noreply.github.com>
2020-01-15 15:20:45 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import Loader from './Loader/Loader'
+import Table from './Table/Table'
+
+class App extends Component {
+
+  state = {
+    isloading: true,
+    data: []
+  }
+
+ async componentDidMount() {
+    const response = await fetch('http://www.filltext.com/?rows=32&id={number|1000}&firstName={firstName}&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}')
+    const data = await response.json()
+    console.log(data)
+
+    this.setState({
+      isLoading: false,
+      data
+    })
+  }
+  render() {
+    return (
+      <div className="container">
+        { 
+          this.state.isLoading 
+            ? <Loader />
+            : <Table 
+              data={this.state.data}
+            />
+        }
+      </div>
+    );
+  }
+}
+
+export default App;
